48eb6270979adfe6fe21b162a7b3953a52a155fa
Justin Collins <justin@presidentbeef.com>
2014-01-23 01:34:38 -0800

    Changes to whiteboard test app

--- examples/whiteboard/test/try.rb
+++ examples/whiteboard/test/try.rb
@@ -1,6 +1,6 @@
 require '../melon_whiteboard'
 
-mwb = MelonWhiteboard.new {}
+mwb = MelonWhiteboard.new(ARGV.pop) do; end
 
 ARGV.each do |port|
   mwb.add_remote port.to_i
@@ -14,14 +14,18 @@ t = Thread.new do
   end
 end
 
+print "Send? "
 x = ($stdin.gets.chomp == "y")
 
-30.times do
-  mwb.add_local_figure(mwb.create_figure({type: :point, x: 1, y: 0}, true)) if x
-  sleep(rand(3) + 2)
-  p mwb.out_of_order
+if x
+  100.times do
+    mwb.add_local_figure(mwb.create_figure({type: :point, x: 1, y: 0}, true))
+    sleep(rand(3) + 2)
+  end
 end
 
-t.join unless x
 
-puts "done"
+print "waiting for you"
+$stdin.gets
+p mwb.out_of_order?
+exit!
