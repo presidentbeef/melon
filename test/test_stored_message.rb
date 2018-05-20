6356e71cc3c305cb2db517c0ff879681c6c02a80
Justin Collins <justin@presidentbeef.com>
2018-05-19 19:30:16 -0700

    Add tests for Melon::StoredMessage

---
+++ test/test_stored_message.rb
@@ -0,0 +1,23 @@
+class TestStoredMessage < Minitest::Test
+  def test_creation
+    m = ["hello", "world"]
+    s = Melon::StoredMessage.new(1, 2, m)
+    
+    assert_kind_of Integer, s.id
+    assert_equal ["hello", "world"], s.message
+    refute_equal m.object_id, s.message.object_id 
+  end
+
+  def test_matching
+    m = ["hello", "world"]
+    s = Melon::StoredMessage.new(1, 2, m)
+
+    assert_match s, [String, String]
+    assert_match s, ["hello", String]
+    assert_match s, [String, "world"]
+    assert_match s, m
+    assert_match s, [Object, Object]
+    refute_match s, [Integer, "world"]
+    refute_match s, [Class, String]
+  end
+end
